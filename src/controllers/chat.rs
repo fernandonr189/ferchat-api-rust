117c5a67676f7c6e97ee75a8c926e2a964ab3ae8
Fernando Ismael Navarrete Ruiz <fernando.navarrete@toka.mx>
2024-10-10 21:25:21 +0000

    added method for websocket communication using only one socket

--- src/controllers/chat.rs
+++ src/controllers/chat.rs
@@ -1,5 +1,10 @@
+use std::sync::Arc;
+use std::task::Context;
+use rocket::tokio;
+use rocket::tokio::sync::Mutex;
 use crate::models::chat_server::ChatServer;
 use crate::models::response::Jwt;
+use rocket::futures::task::noop_waker;
 use rocket::State;
 use rocket::futures::{SinkExt, StreamExt};
 
@@ -93,6 +98,83 @@ pub async fn yell(
             chat_server.get_session_tx(&session_id).await.unwrap()
         },
     };
+
+    let rx = match chat_server.get_session_rx(&session_id).await {
+        Some(rx) => {
+          rx  
+        },
+        None => {
+            chat_server.new_session(&session_id).await;
+            chat_server.get_session_rx(&session_id).await.unwrap()
+        },
+    };
+
+
+    ws.channel(move |stream| {
+        Box::pin(async move {
+
+            let arc_stream = Arc::new(Mutex::new(stream));
+            let stream_clone = Arc::clone(&arc_stream);
+
+            tokio::spawn(async move {
+                let mut rx_lock = rx.lock().await;
+                while let Some(msg) = rx_lock.recv().await {
+                    let mut stream_lock = arc_stream.lock().await;
+                    let _ = stream_lock.send(ws::Message::text(msg));
+                    drop(stream_lock);
+                };
+            });
+            let mut stream_lock = stream_clone.lock().await;
+            while let Some(msg) = stream_lock.next().await {
+                let msg_res = match msg {
+                    Ok(msg) => msg,
+                    Err(_) => ws::Message::text("Error receiving message"),
+                };
+                let _ = tx.send(msg_res.to_string());
+            };
+            
+            Ok(())
+        })
+    })
+}
+
+
+#[get("/chat/<target_id>")]
+pub async fn chat(
+    ws: ws::WebSocket,
+    target_id: i32,
+    jwt: Jwt,
+    chat_server: &State<ChatServer>,
+) -> ws::Channel<'static> {
+    let user_id = jwt.claims.subject_id;
+
+    let session_id = format!(
+        "{}-{}-{}",
+        if user_id < target_id {
+            user_id
+        } else {
+            target_id
+        },
+        if user_id < target_id {
+            target_id
+        } else {
+            user_id
+        },
+        target_id
+    );
+
+    println!("session_id: {}", session_id);
+
+    let tx = match chat_server.get_session_tx(&session_id).await {
+        Some(tx) => {
+          tx  
+        },
+        None => {
+            chat_server.new_session(&session_id).await;
+            chat_server.get_session_tx(&session_id).await.unwrap()
+        },
+    };
+    
 
     ws.channel(move |mut stream| {
         Box::pin(async move {
